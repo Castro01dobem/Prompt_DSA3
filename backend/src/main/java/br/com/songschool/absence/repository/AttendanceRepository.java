d4b6217cb414868ee7a7611d1d0749ed3aa203c1
Castro01dobem <rm95036@estudante.fieb.edu.br>
2026-06-17 11:18:10 -0300

    Aula17.06_2

--- backend/src/main/java/br/com/songschool/absence/repository/AttendanceRepository.java
+++ backend/src/main/java/br/com/songschool/absence/repository/AttendanceRepository.java
@@ -1,14 +1,16 @@
 package br.com.songschool.absence.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import br.com.songschool.absence.domain.Attendance;
 import br.com.songschool.absence.domain.ClassGroup;
 import br.com.songschool.absence.domain.Lesson;
 import br.com.songschool.absence.domain.User;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
   Optional<Attendance> findByLessonAndStudent(Lesson lesson, User student);
@@ -22,3 +24,4 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
   long countByClassGroupAndStudent(@Param("classGroup") ClassGroup classGroup, @Param("student") User student);
 }
 
+//opa
